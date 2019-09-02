21010b5a6b0adb65e27f07730a327cb2eabcfd9b
vaibhav kumar <vaibhav.kumar_cs18@gla.ac.in>
2019-09-02 14:33:10 +0530

    create gatter setter method

--- src/main/Person.java
+++ src/main/Person.java
@@ -1,8 +1,32 @@
 package main;
-
 public class Person {
     private String name;
     private int age;
     private char gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
 
 }
